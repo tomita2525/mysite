817a385671ccd909f4fa4a10a2ac3e67b28eaa0c
tomita2525 <tomita.amane@opst.co.jp>
2019-12-13 18:28:28 +0900

    jsディレクトリ作成

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.login').click(function(){
+      $('#login-modal').fadeIn();
+    });
+    $('.close-modal').click(function(){
+      $('#login-modal').fadeOut();
+    });
+
+});

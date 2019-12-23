439aae394c323285aaeb7dbd87c7a830a68f6634
tomita2525 <tomita.amane@opst.co.jp>
2019-12-23 18:09:39 +0900

    アコーディオン作成

--- dist/js/accordion.js
+++ dist/js/accordion.js
@@ -1,23 +1,43 @@
 
 'use strict';
-console.log("hello!");
+// window.onload = function(){
+window.addEventListener('DOMContentLoaded', function() {
+  console.log("hello!");
 
-const target = document.getElementById('abc');
-target.addEventListener('click', function(){
-  // console.log('クリックされました！');
-  target.classList.toggle('circle')
-});
+  const target = document.getElementById('abc');
+  target.addEventListener('click', function(e){
+    // console.log('クリックされました！');
+    target.classList.toggle('circle');
+    console.log(e);
+  });
 
-const acd_target = document.querySelectorAll('.acd_menu_head');
-
-for(var i = 0; i<acd_target.length; i++){
-  acd_target[i].addEventListener('click', function(){
-
+  const acd_target = document.querySelectorAll('.acd_menu_head');
+  const func = function(){
     const item = this.nextElementSibling;
     if(item.classList.contains('open')){
       item.classList.remove('open');
     } else {
       item.classList.add('open');
     }
+  };
+
+  for(var i = 0; i<acd_target.length; i++){
+    acd_target[i].addEventListener('click', func);
+  }
+
+});
+
+$(function(){
+  console.log("salam!");
+  
+  $('.j_acd_menu_head').click(function() {
+    var $items = $(this).next();
+    if($items.hasClass('open')){
+      $items.removeClass('open');
+      $items.slideUp(3000);
+    } else {
+      $items.addClass('open');
+      $items.slideDown(3000);
+    }
   });
-};
+});

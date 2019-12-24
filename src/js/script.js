4b024e236e53794076a968aeb9169dcad89bb369
tomita2525 <tomita.amane@opst.co.jp>
2019-12-24 17:48:01 +0900

    配置場所を変更

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('.login').click(function(){
-      $('#login-modal').fadeIn();
-    });
-    $('.close-modal').click(function(){
-      $('#login-modal').fadeOut();
-    });
-
-});

7f3f820239517b4f444fce07fb8d790db659b23b
tomita2525 <tomita.amane@opst.co.jp>
2019-12-26 17:40:18 +0900

    jQueryのアコーディオン秒数変更、toggleイベント追加

--- dist/js/accordion.js
+++ dist/js/accordion.js
@@ -1,43 +1,53 @@
-
 'use strict';
 // window.onload = function(){
-window.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
   console.log("hello!");
 
-  const target = document.getElementById('abc');
-  target.addEventListener('click', function(e){
-    // console.log('クリックされました！');
-    target.classList.toggle('circle');
-    console.log(e);
-  });
+  // const target = document.getElementById('abc');
+  // target.addEventListener('click', function(e){
+  //   // console.log('クリックされました！');
+  //   target.classList.toggle('circle');
+  //   console.log(e);
+  // });
 
   const acd_target = document.querySelectorAll('.acd_menu_head');
-  const func = function(){
+  const func = function() {
     const item = this.nextElementSibling;
-    if(item.classList.contains('open')){
+    if (item.classList.contains('open')) {
       item.classList.remove('open');
     } else {
       item.classList.add('open');
     }
   };
 
-  for(var i = 0; i<acd_target.length; i++){
+  for (var i = 0; i < acd_target.length; i++) {
     acd_target[i].addEventListener('click', func);
   }
 
 });
 
-$(function(){
+$(function() {
   console.log("salam!");
-  
+
+  $('.box').click(function() {
+    $(this).toggleClass('circle');
+  });
+
+
   $('.j_acd_menu_head').click(function() {
     var $items = $(this).next();
-    if($items.hasClass('open')){
-      $items.removeClass('open');
-      $items.slideUp(3000);
+    if ($items.hasClass('j_open')) {
+      $items.removeClass('j_open');
+      $items.slideUp(1000);
     } else {
-      $items.addClass('open');
-      $items.slideDown(3000);
+      $items.addClass('j_open');
+      $items.slideDown(1000);
     }
   });
+
+  // img要素にhoverで説明文表示
+  $('.img-hover').hover(function() {
+    var $item = $(this).siblings().find('.text');
+    $(this).siblings('.text').toggle('text-active');
+  }, );
 });

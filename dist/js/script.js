7825c954642b46c2e835bbcfd73087b5c8eb712a
tomita2525 <tomita.amane@opst.co.jp>
2019-12-26 14:52:37 +0900

    インデントを揃えた

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,26 +1,30 @@
-$(function(){
-    $('.login').click(function(){
-      $('#login-modal').fadeIn();
-    });
-    $('.close-modal').click(function(){
-      $('#login-modal').fadeOut();
-    });
+$(function() {
+  $('.login').click(function() {
+    $('#login-modal').fadeIn();
+  });
+  $('.close-modal').click(function() {
+    $('#login-modal').fadeOut();
+  });
 
-    $('.lesson-hover').hover(
-      function(){
-        $(this).find('.txt-contents').addClass('txt-active');
-      },
-      function(){
-        $(this).find('.txt-contents').removeClass('txt-active');
-      }
-    );
+  $('.lesson-hover').hover(
+    function() {
+      $(this).find('.txt-contents').addClass('txt-active');
+    },
+    function() {
+      $(this).find('.txt-contents').removeClass('txt-active');
+    }
+  );
 
-    $(".faq-list-item").click(function(){
-      if($(this).find('.answer').hasClass('open')){
-        removeClass('open');
-      } else {
-        
-      }
+  $(".faq-list-item").click(function() {
+    var $answer = $(this).find('.answer');
+    if ($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      $answer.slideUp(1000);
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown(1000);
 
-    });
+    }
+
+  });
 });
